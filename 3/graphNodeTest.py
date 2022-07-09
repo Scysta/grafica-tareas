73341a8e819542154b70378ec09c41f22786dc05
Scysta <isotericscysta@hotmail.com>
2022-07-09 19:16:44 -0400

    Things

--- 3/graphNodeTest.py
+++ 3/graphNodeTest.py
@@ -1,14 +1,16 @@
 import glfw
+from lib.grafica import basic_shapes as bs
+from lib.grafica import easy_shaders as es
+from lib.grafica import transformations as tr
+from lib.grafica import scene_graph as sg
+from lib.grafica import lighting_shaders as ls
+from lib import catrom
 from OpenGL.GL import *
 import OpenGL.GL.shaders
 import numpy as np
 import sys, os.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from grafica import basic_shapes as bs
-from grafica import easy_shaders as es
-from grafica import transformations as tr
-from grafica import scene_graph as sg
-from lib import catrom
+
 
 class Controller():
     def __init__(self):
@@ -16,14 +18,16 @@ class Controller():
         self.view = 2
         self.fillPolygon = True
 
+
 controller = Controller()
+
 
 def on_key(window, key, scancode, action, mods):
     if action != glfw.PRESS:
         return
 
     global controller
-        
+
     if key == glfw.KEY_R:
         controller.rotation = not controller.rotation
 
@@ -45,59 +49,82 @@ def on_key(window, key, scancode, action, mods):
     elif key == glfw.KEY_Q:
         glfw.set_window_should_close(window, True)
 
-    else: 
+    else:
         print("Unknown key")
 
+
+def createGPUShape(pipeline, shape):
+    gpuShape = es.GPUShape().initBuffers()
+    pipeline.setupVAO(gpuShape)
+    gpuShape.fillBuffers(shape.vertices, shape.indices, GL_STATIC_DRAW)
+    return gpuShape
+
+
 def createColorPyramid(color):
+    sq = np.sqrt(2)
     vertices = [
-            -0.5,  0.0,  0.0, *color,
-             0.5,  0.0,  0.0, *color,
-             0.0,  0.0,  np.sqrt(0.75),  *color,
-            -0.5, -1.0,  0.0, *color,
-             0.5, -1.0,  0.0, *color
-            ]
+        -0.5,  0.0,  0.0,           *color, 0, 1, 0,
+         0.5,  0.0,  0.0,           *color, 0, 1, 0,
+         0.0,  0.0,  np.sqrt(0.75), *color, 0, 1, 0,
+
+        -0.5, -1.0,  0.0,           *color, 0, 0, 1,
+         0.5, -1.0,  0.0,           *color, 0, 0, 1,
+         0.5,  0.0,  0.0,           *color, 0, 0, 1,
+        -0.5,  0.0,  0.0,           *color, 0, 0, 1,
+
+         0.5,  0.0,  0.0,           *color, np.cos(30), 0, np.sin(30),
+         0.5, -1.0,  0.0,           *color, np.cos(30), 0, np.sin(30),
+         0.0,  0.0,  np.sqrt(0.75), *color, np.cos(30), 0, np.sin(30),
+
+         0.5, -1.0,  0.0,           *color, 0, -sq * np.sin(32), sq * np.cos(32),
+        -0.5, -1.0,  0.0,           *color, 0, -sq * np.sin(32), sq * np.cos(32),
+         0.0,  0.0,  np.sqrt(0.75), *color, 0, -sq * np.sin(32), sq * np.cos(32),
+
+        -0.5, -1.0,  0.0,           *color, -np.cos(30), 0, np.sin(30),
+        -0.5,  0.0,  0.0,           *color, -np.cos(30), 0, np.sin(30),
+         0.0,  0.0,  np.sqrt(0.75), *color, -np.cos(30), 0, np.sin(30),
+    ]
+
     indices = [
-            0, 1, 2,
-            3, 4, 1,
-            1, 0, 3,
-            4, 2, 1,
-            3, 0, 2,
-            4, 3, 2
-            ]
+        0, 1, 2,
+        3, 4, 5,
+        5, 6, 3,
+        7, 8, 9,
+        10, 11, 12,
+        13, 14, 15
+    ]
 
     return bs.Shape(vertices, indices)
+
 
 def createBlueQuad():
-    color1 = [135/255, 206/255, 235/255]
+    color1 = [135 / 255, 206 / 255, 235 / 255]
     color2 = [0,       191/255, 1      ]
     vertices = [
-            -1.0,  0.0, -1.0, *color1,
-             1.0,  0.0, -1.0, *color1,
-             1.0,  0.0,  1.0, *color2,
-            -1.0,  0.0,  1.0, *color2
-            ]
+        -1.0,  0.0, -1.0, *color1, 0, 1, 0,
+         1.0,  0.0, -1.0, *color1, 0, 1, 0,
+         1.0,  0.0,  1.0, *color2, 0, 1, 0,
+        -1.0,  0.0,  1.0, *color2, 0, 1, 0
+    ]
 
     indices = [
-            0, 1, 2, 2, 3, 0
-            ]
+        0, 1, 2, 2, 3, 0
+    ]
 
     return bs.Shape(vertices, indices)
 
+
 def createBoat(pipeline):
-    color = [139/255, 69/255, 19/255]
+    color = [139 / 255, 69 / 255, 19 / 255]
 
     # Figuras en memoria
-    brownCube = bs.createColorCube(*color)
-    gpuBrownCube = es.GPUShape().initBuffers()
-    pipeline.setupVAO(gpuBrownCube)
-    gpuBrownCube.fillBuffers(brownCube.vertices, brownCube.indices, GL_STATIC_DRAW)
+    brownCube = bs.createColorNormalsCube(*color)
+    gpuBrownCube = createGPUShape(pipeline, brownCube)
 
     brownPyramid = createColorPyramid(color)
-    gpuBrownPyramid = es.GPUShape().initBuffers()
-    pipeline.setupVAO(gpuBrownPyramid)
-    gpuBrownPyramid.fillBuffers(brownPyramid.vertices, brownPyramid.indices, GL_STATIC_DRAW)
+    gpuBrownPyramid = createGPUShape(pipeline, brownPyramid)
 
-    #blackCylinder = 
+    #blackCylinder =
     #gpublackCylinder = es.GPUShape().initBuffers()
     #pipeline.setupVAO(gpublackCylinder)
     #gpublackCylinder.fillBuffers(blackCylinder.vertices, blackCylinder.indices. GL_STATIC_DRAW)
@@ -110,16 +137,16 @@ def createBoat(pipeline):
     # Proa y popa
     proa = sg.SceneGraphNode("proa")
     proa.transform = tr.matmul([
-        tr.translate(1, 0.5, 0), 
-        tr.rotationY(np.pi/2)
-        ])
+        tr.translate(1, 0.5, 0),
+        tr.rotationY(np.pi / 2)
+    ])
     proa.childs += [gpuBrownPyramid]
 
     popa = sg.SceneGraphNode("popa")
     popa.transform = tr.matmul([
-        tr.translate(-1, 0.5, 0), 
-        tr.rotationY(-np.pi/2)
-        ])
+        tr.translate(-1, 0.5, 0),
+        tr.rotationY(-np.pi / 2)
+    ])
     popa.childs += [gpuBrownPyramid]
 
     # Mástil
@@ -147,12 +174,11 @@ def createBoat(pipeline):
 
     return barcoVista
 
+
 def createEnvironment(pipeline):
     # Figuras a memoria
     blueQuad = createBlueQuad()
-    gpuBlueQuad = es.GPUShape().initBuffers()
-    pipeline.setupVAO(gpuBlueQuad)
-    gpuBlueQuad.fillBuffers(blueQuad.vertices, blueQuad.indices, GL_STATIC_DRAW)
+    gpuBlueQuad = createGPUShape(pipeline, blueQuad)
 
     # Mar
     mar = sg.SceneGraphNode("mar")
@@ -160,13 +186,14 @@ def createEnvironment(pipeline):
 
     # Isla
     #isla = sg.SceneGraphNode("isla")
-    #isla.childs 
+    #isla.childs
 
     # Ambiente
     ambiente = sg.SceneGraphNode("ambiente")
     ambiente.childs += [mar]
 
     return ambiente
+
 
 def createScene(pipeline):
     barcoTras = sg.SceneGraphNode("barcoTras")
@@ -199,9 +226,12 @@ if __name__ == "__main__":
     # Conectamos on_key a la ventana
     glfw.set_key_callback(window, on_key)
 
-    # Elejimos el shader y le decimos a OpenGL que lo use
-    pipeline = es.SimpleModelViewProjectionShaderProgram()
-    glUseProgram(pipeline.shaderProgram) 
+    # Elegimos el shader y le decimos a OpenGL que lo use
+    pipeline = ls.SimplePhongShaderProgram()
+    glUseProgram(pipeline.shaderProgram)
+
+    # Lighting shaders
+    lightingPipeline = ls.SimplePhongShaderProgram()
 
     # Color del background
     glClearColor(0.6, 0.6, 0.6, 1.0)
@@ -212,7 +242,7 @@ if __name__ == "__main__":
     # Creamos el grafo de escena
     sceneNode = createScene(pipeline)
 
-    # Seteamos proyección 
+    # Seteamos proyección
     projection = tr.perspective(90, float(width) / float(height), 0.1, 100)
     glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
 
@@ -232,15 +262,15 @@ if __name__ == "__main__":
         vert = [float(x) for x in line.split()]
         vertices += [vert]
 
-    # 900 fps 
-    trayectoria = catrom.getSplineFixed(vertices, 100)
+    # 900 fps
+    trayectoria = catrom.getSplineFixed(vertices, 130)
 
     numVertices = len(trayectoria)
     index = 0
     x0, y0 = trayectoria[0][0], trayectoria[0][1]
 
     traslatedBoatNode = sg.findNode(sceneNode, "barcoTras")
-    traslatedBoatNode.transform = tr.translate(trayectoria[0][0], 0, trayectoria[0][1])
+    traslatedBoatNode.transform = tr.translate(x0, 0, y0)
 
     while not glfw.window_should_close(window):
 
@@ -264,34 +294,53 @@ if __name__ == "__main__":
         boatTrayectoryNode.transform = tr.translate(x, 0, y)
 
         # Modificar hacia dónde mira el barco
-        x1, y1 = x-x0, y-y0
-        theta = np.arctan2(x1, y1) + np.pi/2
+        x1, y1 = x - x0, y - y0
+        theta = np.arctan2(x1, y1) + np.pi / 2
         boatFacingNode = sg.findNode(sceneNode, "barcoVista")
         boatFacingNode.transform = tr.rotationY(theta)
         x0, y0 = x, y
 
-        # 4 distintas cámaras 
+        # 4 distintas cámaras
         match controller.view:
             case 1:
                 view = tr.lookAt(
                     np.array([x, 0.1, y]),
                     np.array([x1, 0.1, y1]),
                     np.array([0, 1, 0])
-                    )
-                glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "view"), 1, GL_TRUE, view)
-
+                )
+                viewPos = np.array([x, 0.1, y])
             case 2:
                 view = tr.lookAt(
-                    np.array([-2, 3, -3]),
+                    np.array([-1, 1, -2]),
                     np.array([0, 0, 0]),
                     np.array([0, 1, 0])
-                    )
-                glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "view"), 1, GL_TRUE, view)
-
+                )
+                viewPos = np.array([-1, 1, -2])
             #case 3:
 
-
             #case 4:
+
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "view"), 1, GL_TRUE, view)
+
+        # Lighting parameters
+
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "La"), 1.0, 1.0, 1.0)
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "Ld"), 1.0, 1.0, 1.0)
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "Ls"), 1.0, 1.0, 1.0)
+
+        # Object is barely visible at only ambient. Diffuse behavior is slightly red. Sparkles are white
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "Ka"), 0.5, 0.5, 0.5)
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "Kd"), 0.2, 0.5, 0.5)
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "Ks"), 1.0, 1.0, 1.0)
+
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "lightPosition"), -5, 5, 5)
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "viewPosition"), viewPos[0], viewPos[1],
+                    viewPos[2])
+        glUniform1ui(glGetUniformLocation(lightingPipeline.shaderProgram, "shininess"), 50)
+
+        glUniform1f(glGetUniformLocation(lightingPipeline.shaderProgram, "constantAttenuation"), 0.0001)
+        glUniform1f(glGetUniformLocation(lightingPipeline.shaderProgram, "linearAttenuation"), 0.03)
+        glUniform1f(glGetUniformLocation(lightingPipeline.shaderProgram, "quadraticAttenuation"), 0.01)
 
         # Dibujamos la escena
         sg.drawSceneGraphNode(sceneNode, pipeline, "model")
@@ -308,4 +357,3 @@ if __name__ == "__main__":
     sceneNode.clear()
 
     glfw.terminate()
-
